adbe19a9b3604f7eae284e1c71ef76c9c938b341
xemcerk <lisislzx@sina.com>
2020-02-28 21:22:39 +0800

    add database readme

--- spider_project/com_info_spider/spiders/selenium_spider.py
+++ spider_project/com_info_spider/spiders/selenium_spider.py
@@ -7,9 +7,11 @@ from scrapy_selenium import SeleniumRequest
 class SeleniumSpider(scrapy.Spider):
     name = "selenium_spider"
 
-    start_urls = ['https://www.tianyancha.com']
+    def start_requests(self):
+        yield SeleniumRequest(url='https://www.tianyancha.com', callback=self.parse)
 
     def parse(self, response):
-        inspect_response(response)
+        # print(response.meta['driver'].title)
+        inspect_response(response, self)
 
 
