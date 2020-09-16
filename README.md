f7361e4142da7680a05fd07f88b18d5d5ed6e820
xemcerk <lisislzx@sina.com>
2020-09-16 15:29:47 +0800

    update mongodb server address

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ import pymongo
 import pandas as pd
 
 # 连接到mongo server
-client = pymongo.MongoClient(host='10.108.210.101')
+client = pymongo.MongoClient(host='49.234.215.201')
 
 # 提取数据库TechBigData中 集合CompanyProfile的所有数据
 df = pd.DataFrame(list(client.TechBigData.CompanyProfile.find()))
--- lda_on_all_records.py
+++ lda_on_all_records.py
@@ -1,27 +1,33 @@
 import pymongo
 import pandas as pd
+import jieba
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.decomposition import LatentDirichletAllocation
 
-client = pymongo.MongoClient(host='10.108.210.101')
+# 载入经营范围数据
+client = pymongo.MongoClient(host='49.234.215.201')
 df = pd.DataFrame(list(client.TechBigData.CompanyProfile.find({},{"businessScope":1})))
 scope_str_list = df['businessScope'].dropna().to_list()
 
-import jieba
+# 分词
 cut_docs = [jieba.lcut(doc) for doc in scope_str_list]
 cut_docs = [" ".join(cut_doc) for cut_doc in cut_docs]
 
+# 去停用词
 with open('zh_stop_words\\stop_words.txt','r',encoding='utf-8') as f:
     stop_words = (f.readlines())
 stop_words = [stop_word.strip('\n') for stop_word in stop_words]
 
-from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+# tfidf向量
 tf_vectorizer = CountVectorizer(max_df=0.95, min_df=2, stop_words=stop_words)
 tf = tf_vectorizer.fit_transform(cut_docs)
 tf_feature_names = tf_vectorizer.get_feature_names()
 
-from sklearn.decomposition import LatentDirichletAllocation
+# LDA主题建模
 lda = LatentDirichletAllocation(n_components=6, max_iter=100, random_state=42).fit(tf)
 doc_topic = lda.transform(tf)
 
+# 将各文档主题向量存入数据库
 com_prfl = client.TechBigData.CompanyProfile
 id_list = list(com_prfl.find({},{"_id":1}))
 for i, vec in enumerate(doc_topic):
